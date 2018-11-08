c2b168c20c5f0976af350a6523c4f9b43b9a7681
Nathan Ringo <remexre@gmail.com>
2018-11-08 13:16:07 -0600

    Fixed weird decoration bug...

--- edu.umn.cs.melt.minidep/abstractsyntax/unification/Normalization.sv
+++ edu.umn.cs.melt.minidep/abstractsyntax/unification/Normalization.sv
@@ -5,18 +5,11 @@ import edu:umn:cs:melt:minidep:util;
 autocopy attribute inhValEnv :: [Pair<String Maybe<Expr>>] occurs on Decl, Decls, Expr;
 synthesized attribute normal :: Boolean occurs on Expr;
 synthesized attribute normalized :: Expr occurs on Expr;
-synthesized attribute reduce :: Expr occurs on Expr;
 
 aspect production declsCons
 top::Decls ::= h::Decl t::Decls
 {
   t.inhValEnv = h.synValEnv ++ top.inhValEnv;
-}
-
-aspect default production
-top::Expr ::=
-{
-  top.normalized = if top.normal then top else top.reduce.normalized;
 }
 
 aspect production app
@@ -26,51 +19,54 @@ top::Expr ::= f::Expr x::Expr
   | lam(_, _) -> false
   | _ -> f.normal
   end;
-  top.reduce = if x.normal then
+
+  local reduced :: Expr = if x.normal then
     if f.normal then
       case f of
       | lam(n, b) -> b.beta(n, x)
       | _ -> top
       end
     else
-      app(f.reduce, x, location=top.location)
+      app(f.normalized, x, location=top.location)
   else
-    app(f, x.reduce, location=top.location);
+    app(f, x.normalized, location=top.location);
+  reduced.inhValEnv = top.inhValEnv;
+  top.normalized = if reduced.normal then reduced else reduced.normalized;
 }
 
 aspect production lam
 top::Expr ::= name::String body::Expr
 {
   top.normal = true;
-  top.reduce = top;
+  top.normalized = top;
 }
 
 aspect production pi
 top::Expr ::= name::Maybe<String> l::Expr r::Expr
 {
   top.normal = true;
-  top.reduce = top;
+  top.normalized = top;
 }
 
 aspect production unificationVar
 top::Expr ::= id::Integer
 {
   top.normal = true;
-  top.reduce = top;
+  top.normalized = top;
 }
 
 aspect production universe
 top::Expr ::=
 {
   top.normal = true;
-  top.reduce = top;
+  top.normalized = top;
 }
 
 aspect production var
 top::Expr ::= s::String
 {
   top.normal = !lookupEnv(s, top.inhValEnv).isJust;
-  top.reduce = case lookupEnv(s, top.inhValEnv) of
+  top.normalized = case lookupEnv(s, top.inhValEnv) of
   | just(x) -> x
   | nothing() -> top
   end;
